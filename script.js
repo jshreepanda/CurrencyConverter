b1bab2f0983b60626a498eca82bfbd9074dbc9db
Jayashree <jshreepanda.ca@gmail.com>
2021-02-01 13:59:42 -0500

    By me

---
+++ script.js
@@ -0,0 +1,37 @@
+let currencyElOne = document.getElementById('currency-one')
+let amountElOne = document.getElementById('amount-one')
+let currencyElTwo = document.getElementById('currency-two')
+let amountElTwo = document.getElementById('amount-two')
+
+let rateEl = document.getElementById('rate')
+let swap = document.getElementById('swap')
+
+function calculate() {
+    let myKey = '4e79f8c72bbbe8c6a38c1494'
+    let currency_one = currencyElOne.value
+    let currency_two = currencyElTwo.value
+    fetch(`https://prime.exchangerate-api.com/v5/${myKey}/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(x => {
+        // console.log(x.conversion_rates)
+
+        let rate = x.conversion_rates[currency_two]
+        
+        rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`
+
+        amountElTwo.value = (amountElOne.value * rate).toFixed(2)
+    })
+}
+
+currencyElOne.addEventListener('change', calculate);
+amountElOne.addEventListener('input', calculate);
+currencyElTwo.addEventListener('change', calculate);
+amountElTwo.addEventListener('input', calculate);
+
+swap.addEventListener('click', function() {
+    let temporary = currencyElOne.value;
+    currencyElOne.value = currencyElTwo.value;
+    currencyElTwo.value = temporary;
+})
+
+calculate()
