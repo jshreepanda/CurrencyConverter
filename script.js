4ac74e0a6d7ba36f592e93ab47d705f70ce9b4f7
Jayashree <jshreepanda.ca@gmail.com>
2021-04-27 11:43:51 -0400

    modified bg color, color

--- script.js
+++ script.js
@@ -6,21 +6,24 @@ let amountElTwo = document.getElementById('amount-two')
 let rateEl = document.getElementById('rate')
 let swap = document.getElementById('swap')
 
+// https://v6.exchangerate-api.com/v6/4e79f8c72bbbe8c6a38c1494/latest/USD
+// `https://prime.exchangerate-api.com/v5/${myKey}/latest/${currency_one}
+
 function calculate() {
     let myKey = '4e79f8c72bbbe8c6a38c1494'
     let currency_one = currencyElOne.value
     let currency_two = currencyElTwo.value
-    fetch(`https://prime.exchangerate-api.com/v5/${myKey}/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(x => {
-        // console.log(x.conversion_rates)
+    fetch(`https://v6.exchangerate-api.com/v6/${myKey}/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(x => {
+            console.log(x)
 
-        let rate = x.conversion_rates[currency_two]
-        
-        rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`
+            // let rate = x.conversion_rates[currency_two]
 
-        amountElTwo.value = (amountElOne.value * rate).toFixed(2)
-    })
+            // rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`
+
+            // amountElTwo.value = (amountElOne.value * rate).toFixed(2)
+        })
 }
 
 currencyElOne.addEventListener('change', calculate);
@@ -28,7 +31,7 @@ amountElOne.addEventListener('input', calculate);
 currencyElTwo.addEventListener('change', calculate);
 amountElTwo.addEventListener('input', calculate);
 
-swap.addEventListener('click', function() {
+swap.addEventListener('click', function () {
     let temporary = currencyElOne.value;
     currencyElOne.value = currencyElTwo.value;
     currencyElTwo.value = temporary;
